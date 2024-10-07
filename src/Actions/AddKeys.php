20e518fe7cd679343ca7b0e7a738dc908f289f45
msamgan <mohdsamgankhan@gmail.com>
2024-10-07 12:55:07 -0400

    adding keys with preserving space at the end of the file.

--- src/Actions/AddKeys.php
+++ src/Actions/AddKeys.php
@@ -12,6 +12,11 @@ class AddKeys
             $filePath = base_path($missingKey['envFile']);
             $envContent = file($filePath);
 
+            $lineDiff = count($envContent) - $missingKey['line'];
+            if ($lineDiff < 0) {
+                $envContent = $this->appendEmptyLines(file: $filePath, numberOfLines: abs($lineDiff));
+            }
+
             array_splice(
                 $envContent, $missingKey['line'] - 1, 0, $missingKey['key'] . '=""' . PHP_EOL
             );
@@ -22,5 +27,17 @@ class AddKeys
 
             file_put_contents($filePath, $envContent);
         });
+    }
+
+    private function appendEmptyLines(string $file, int $numberOfLines = 1): array
+    {
+        $envContent = file($file);
+        $lastLine = count($envContent);
+
+        for ($i = 0; $i < $numberOfLines; $i++) {
+            array_splice($envContent, $lastLine, 0, PHP_EOL);
+        }
+
+        return $envContent;
     }
 }
